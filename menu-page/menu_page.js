aa1bb4481646a9e3cabd3e0a4ac059fc1cea8438
Aaron M <amccully@ucsd.edu>
2024-04-26 01:35:05 -0700

    Responsive changes, still needs some polishing

--- menu-page/menu_page.js
+++ menu-page/menu_page.js
@@ -1,43 +1,74 @@
 // Define menu item objects
 const puffleItemsData = [
     {
-        imgSrc: "/CCC-boba-bar/menu-page/assets/menu/Classy.svg",
+        imgSrc: "./assets/menu/Classy.svg",
         title: "Classy Puffle",
         price: "$9",
         description: "Plain puffle, vanilla custard, drizzle"
     },
     {
-        imgSrc: "/CCC-boba-bar/menu-page/assets/menu/Cereal Milk.svg",
+        imgSrc: "./assets/menu/Cereal Milk.svg",
         title: "Cereal Milk",
         price: "$10.25",
         description: "Plain puffle, vanilla custard, fruity pebbles, frosted flakes, cookie crisps, condensed milk"
     },
     {
-        imgSrc: "/CCC-boba-bar/menu-page/assets/menu/Paradise.svg",
+        imgSrc: "./assets/menu/Paradise.svg",
         title: "Paradise",
         price: "$10.25",
         description: "Plain puffle, swirl of vanilla & dole whip, strawberries, bananas, mangoes, honey drizzle"
     },
     {
-        imgSrc: "/CCC-boba-bar/menu-page/assets/menu/Perfect matcha.svg",
+        imgSrc: "./assets/menu/Perfect matcha.svg",
         title: "Perfect Matcha",
         price: "$11",
         description: "Green tea puffle, green tea & vanilla custard, red bean, mochi, pocky sticks, condensed milk drizzle"
     },
     {
-        imgSrc: "/CCC-boba-bar/menu-page/assets/menu/Strawberry Cheesecake.svg",
+        imgSrc: "./assets/menu/Strawberry Cheesecake.svg",
         title: "Strawberry Cheesecake",
         price: "$10.25",
         description: "Plain puffle, vanilla custard, graham crackers, cheesecake bites, strawberries & pocky sticks"
     },
     {
-        imgSrc: "/CCC-boba-bar/menu-page/assets/menu/Smores drizzle.svg",
+        imgSrc: "./assets/menu/Smores drizzle.svg",
         title: "Do you want S’more?",
         price: "$11",
         description: "Chocolate chip puffle, chocolate & vanilla custard, graham crackers, marshmallows, chocolate drizzle"
+    },
+    {
+        imgSrc: "./assets/menu/Brookie Puffle.svg",
+        title: "Brookie Puffle",
+        price: "$11.50",
+        description: "Oreo chocolate chip puffle, cookies and cream & vanilla custard, brownie bites, cookie crisps, chocolate pocky & nutella drizzle"
     }
 ];
 
+// Function to dynamically create item cards
+// function createMenuItemCard(item) {
+//     const itemCard = document.createElement("div");
+//     itemCard.classList.add("item_card");
+
+//     const img = document.createElement("img");
+//     img.src = item.imgSrc;
+//     img.alt = item.title;
+
+//     const title = document.createElement("div");
+//     title.classList.add("item_title");
+//     title.textContent = `${item.title} ${item.price}`;
+
+//     const description = document.createElement("p");
+//     description.classList.add("item_desc");
+//     description.textContent = item.description;
+
+//     itemCard.appendChild(img);
+//     itemCard.appendChild(title);
+//     itemCard.appendChild(description);
+
+//     return itemCard;
+// }
+
+// try this
 // Function to dynamically create item cards
 function createMenuItemCard(item) {
     const itemCard = document.createElement("div");
@@ -47,6 +78,9 @@ function createMenuItemCard(item) {
     img.src = item.imgSrc;
     img.alt = item.title;
 
+    const titleAndDescription = document.createElement("div");
+    titleAndDescription.classList.add("title_and_description");
+
     const title = document.createElement("div");
     title.classList.add("item_title");
     title.textContent = `${item.title} ${item.price}`;
@@ -55,12 +89,15 @@ function createMenuItemCard(item) {
     description.classList.add("item_desc");
     description.textContent = item.description;
 
+    titleAndDescription.appendChild(title);
+    titleAndDescription.appendChild(description);
+
     itemCard.appendChild(img);
-    itemCard.appendChild(title);
-    itemCard.appendChild(description);
+    itemCard.appendChild(titleAndDescription);
 
     return itemCard;
 }
+
 
 // Function to load menu items dynamically
 function loadPuffleItems() {
